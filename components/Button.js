8ee6ba6f0b022054b27830b395f58eb3a8fe0b8e
mathesis <mat@thesis.local>
2023-06-27 21:34:24 +0200

    Dodanie napisu Szukam...

--- components/Button.js
+++ components/Button.js
@@ -2,9 +2,10 @@ const Button = ({ disabled }) => {
   return (
     <button
       type="submit"
+      disabled={disabled}
       className="font-serif text-md shadow-md text-indigo-900 bg-indigo-500 hover:text-indigo-950 p-2 rounded-md"
     >
-      {disabled ? "Ładowanie..." : "Szukaj"}
+      {disabled ? "Szukam..." : "Szukaj"}
     </button>
   );
 };
